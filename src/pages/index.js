7e5d4ccf793c6ef9f9e339ffe7775f4ca7a6b8b4
BojanIstvancic <bojan.istvancic@gmail.com>
2021-04-17 19:21:12 +0200

    add global css, home and project modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import * as styles from "../styles/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
-          <h1>Design</h1>
-          <h2>Develop & Deploy</h2>
-          <p>UX designer & web developer based in Serbia</p>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>UX designer & web developer based in Manchester.</p>
+          <Link className={styles.btn} to="/projects">
+            My Portfolio Projects
+          </Link>
         </div>
       </section>
     </Layout>
