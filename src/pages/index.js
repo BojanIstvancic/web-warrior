a1c2d20221710e308af2d737b55cd43adf6ddabb
BojanIstvancic <bojan.istvancic@gmail.com>
2021-04-18 19:24:04 +0200

    test queries inside components that are not pages*

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
+import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
 
-export default function Home({ data }) {
-  const { title, description } = data.site.siteMetadata
+export default function Home() {
   return (
     <Layout>
       <section className={styles.header}>
@@ -17,21 +16,7 @@ export default function Home({ data }) {
           </Link>
         </div>
         <img src="/banner.png" alt="site banner" style={{ maxWidth: "100%" }} />
-        <p>
-          {title} - {description}
-        </p>
       </section>
     </Layout>
   )
 }
-
-export const query = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-        description
-      }
-    }
-  }
-`
