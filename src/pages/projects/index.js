9b189bc6d52251fd654f3eab5217932223781ef6
BojanIstvancic <bojan.istvancic@gmail.com>
2021-04-19 13:10:28 +0200

    add md file for every project, pull the data and images into project page

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,9 +2,9 @@ import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
 import * as styles from "../../styles/projects.module.css"
+import Img from "gatsby-image"
 
 export default function Projects({ data }) {
-  console.log(data)
   const projects = data.projects.nodes
   const contact = data.contact.siteMetadata.contact
   return (
@@ -16,6 +16,7 @@ export default function Projects({ data }) {
           {projects.map(project => (
             <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
               <div>
+                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
                 <h3>{project.frontmatter.title}</h3>
                 <p>{project.frontmatter.stack}</p>
               </div>
@@ -38,6 +39,13 @@ export const query = graphql`
           slug
           stack
           title
+          thumb {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
         }
         id
       }
